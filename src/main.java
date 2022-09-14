d41e19bd841c75f614a4cc858952674252fa3db6
detayaa <drugochenko.taya@mail.ru>
2022-09-14 21:55:59 +0700

    2

--- src/main.java
+++ src/main.java
@@ -1,13 +1,30 @@
+import java.sql.SQLOutput;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("задание 1");
-        int[] i = {1,2,3};
+        int[] i1 = {1,2,3};
         float[] r = {1.57f,7.654f,9.986f};
-        int[] m = new int[32];
+        int[] m = new int[4];
 
 
-
-
-
+        System.out.println("задание 2");
+        for (int i = 0; i < i1.length; i++) {
+            System.out.print(i1[i] + ", ");
+        }
+        for (int i = 0; i < r.length; i++) {
+            System.out.print(r[i] + ", ");
+        }
+        for (int i = 0; i < m.length; i++) {
+            System.out.print(m[i] + ", ");
+        }
     }
+
 }
+
+
+
+
+
+
+
