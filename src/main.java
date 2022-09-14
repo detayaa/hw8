e56ac24b7386fd2f7c56bd4e8d0fb002e013d4c9
detayaa <drugochenko.taya@mail.ru>
2022-09-14 21:32:08 +0700

    1

--- src/main.java
+++ src/main.java
@@ -1,5 +1,13 @@
 public class main {
     public static void main(String[] args) {
+        System.out.println("задание 1");
+        int[] i = {1,2,3};
+        float[] r = {1.57f,7.654f,9.986f};
+        int[] m = new int[32];
+
+
+
+
 
     }
 }
